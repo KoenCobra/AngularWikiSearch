a20153f0dc8ff97acd6aabb33f17892bb9209fbd
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-21 16:51:31 +0200

    Added table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,14 @@ import {WikipediaService} from "./wikipedia.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  pages = [];
+
   constructor(private wikiService: WikipediaService) {
   }
 
   onTerm(term: string) {
-    this.wikiService.search(term).subscribe((response)=>{
-      console.log(response)
+    this.wikiService.search(term).subscribe((response: any) => {
+      this.pages = response.query.search;
     })
   }
 }
--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
+export interface page {
+  title: string,
+  wordcount: number,
+  snippet: string
+}
 @Component({
   selector: 'app-page-list',
   templateUrl: './page-list.component.html',
   styleUrls: ['./page-list.component.scss']
 })
 export class PageListComponent {
-
+  @Input() pages:page[] = [];
 }
